b8ae41d264f493e84259c539f461f5810066e181
Wenderson-ads <asterty15@gmail.com>
2022-11-04 17:17:22 -0300

    PI terceiro

---
+++ oficinaco-jpa-main/src/main/java/com/example/oficinaco/jpa/entidade/OrdemServicoServico.java
@@ -0,0 +1,58 @@
+package com.example.oficinaco.jpa.entidade;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class OrdemServicoServico {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne(optional = false)
+	private Produto produto;
+	
+	private Integer quantidade;
+	
+	private BigDecimal preco;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+	
+}
