29bc888a9d801b7807518d2c4292e6d36f594f81
witorsather <satherws@gmail.com>
2022-11-23 21:19:26 -0300

    atualizacao

---
+++ oficina-mecanica-jpa/src/main/java/com/example/oficinamecanicajpa/OficinaMecanicaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.oficinamecanicajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaMecanicaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaMecanicaJpaApplication.class, args);
+	}
+
+}
---
+++ oficina-mecanica-jpa/src/test/java/com/example/oficinamecanicajpa/OficinaMecanicaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oficinamecanicajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaMecanicaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
