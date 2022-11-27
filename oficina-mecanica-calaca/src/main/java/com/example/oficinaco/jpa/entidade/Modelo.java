7c352b582d955d743b76d4b9fe7064ca384dd8ea
Wenderson-ads <asterty15@gmail.com>
2022-11-27 18:30:49 -0300

    teste

--- oficina-mecanica-calaca/src/main/java/com/example/oficinaco/jpa/entidade/Modelo.java
+++ oficina-mecanica-calaca/src/main/java/com/example/oficinaco/jpa/entidade/Modelo.java
@@ -4,10 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
-import com.example.oficinaco.jpa.dao.*;
+
+
 
 @Entity
 public class Modelo {
