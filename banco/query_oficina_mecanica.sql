16d9a3f22e44382ff4e7f9bbd0ea14465e7177dd
witorsather <satherws@gmail.com>
2022-12-01 14:29:13 -0300

    teste

--- banco/query_oficina_mecanica.sql
+++ banco/query_oficina_mecanica.sql
@@ -2,34 +2,26 @@
 drop table IF exists municipio cascade;
 drop table IF exists endereco cascade;
 drop table IF exists pessoa cascade;
-
 drop table IF exists marca cascade;
 drop table IF exists modelo cascade;
 drop table IF exists veiculo cascade;
-
 drop table IF exists ordem_servico_produto  cascade;
 drop table IF exists produto cascade;
-
 drop table IF exists ordem_servico_servico cascade;
 drop table IF exists ordem_servico cascade;
-
 drop table IF exists servico cascade;
 
 --selecionar todas as tabelas do banco
 select * from municipio m;
 select * from endereco e;
 select * from pessoa p;
-
 select * from marca ma;
 select * from modelo mo;
 select * from veiculo ve;
-
 select * from ordem_servico_produto osp;
 select * from produto pr;
-
 select * from ordem_servico_servico oss;
 select * from servico se;
-
 select * from ordem_servico os;
 
 --popular todas as tabelas do banco
@@ -71,7 +63,6 @@ INSERT INTO pessoa (cpf, funcionario, nome, telefones, whatsapp)
 VALUES ('40223146545', false, 'Fernando Diego', '62997563458', false);
 INSERT INTO pessoa (cpf, funcionario, nome, telefones, whatsapp)
 VALUES ('60223146545', false, 'Julia de Oliveira', '62997563458', false);
-
 select * from endereco p;
 truncate endereco cascade;
 INSERT INTO endereco (bairro, cep, complemento, logradouro, numero, municipio_id)
@@ -110,7 +101,6 @@ INSERT INTO endereco (bairro, cep, complemento, logradouro, numero, municipio_id
 VALUES ('São Francisco', '74445661', 'QD. 4B LT. 25', 'Rua Ferreira', '505', 2174);
 INSERT INTO endereco (bairro, cep, complemento, logradouro, numero, municipio_id)
 VALUES ('Centro', '74445661', 'QD. 4B LT. 25', 'Rua Ferreira', '5', 2174);
-
 select * from marca ma;
 select * from modelo mo;
 select * from veiculo ve;
