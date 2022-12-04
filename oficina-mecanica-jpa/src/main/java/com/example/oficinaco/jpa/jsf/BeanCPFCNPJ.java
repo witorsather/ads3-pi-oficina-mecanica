bf35e0cb56c483e6a394ceab1b053c410d1f37cb
Bruno Eduardo <cabeca_bru1@hotmail.com>
2022-12-04 01:30:56 -0300

    mascara CPF/CNPJ

---
+++ oficina-mecanica-jpa/src/main/java/com/example/oficinaco/jpa/jsf/BeanCPFCNPJ.java
@@ -0,0 +1,34 @@
+package com.example.oficinaco.jpa.jsf;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class BeanCPFCNPJ {
+    private String opcao;
+    private String campo;
+
+    public boolean isCPF(){
+        return getOpcao()!=null && getOpcao().equals("CPF");
+    }
+    public boolean isCNPJ(){
+        return getOpcao()!=null && getOpcao().equals("CNPJ");
+    }
+ 
+    public String getOpcao() {
+        return opcao;
+    }
+ 
+    public void setOpcao(String opcao) {
+        this.opcao = opcao;
+    }
+ 
+    public String getCampo() {
+        return campo;
+    }
+ 
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+}
